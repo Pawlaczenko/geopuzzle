a83b76e40a789790a0309e4bbb1251f15494da67
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2023-05-18 14:41:28 +0200

    GEOP-21 Add Input component

--- client/src/routes/CreateTrack/CreateTrack.tsx
+++ client/src/routes/CreateTrack/CreateTrack.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react'
 import Banner from 'src/components/Banner'
+import Input from 'src/components/Input/Input'
 import Page from 'src/layout/Page.styled'
+import Section from 'src/layout/Section.styled'
 
 const CreateTrack : FC = () => {
   return (
     <Page>
         <Banner text="Stwórz Trasę" />
+        <Section>
+            <Input label='Nazwa Trasy' name='track_name' placeholder='Podaj nazwę trasy' required={true}  />
+            <Input label='Opis Trasy' name='track_description' placeholder='Podaj opis trasy' required={true} type='textarea' />
+        </Section>
     </Page>
   )
 }
