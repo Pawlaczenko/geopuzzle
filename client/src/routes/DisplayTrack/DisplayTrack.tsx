8e0ba25bb6ef6a5f22c2435e52aabd9976a92407
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2023-07-13 16:15:06 +0200

    GEOP-97 Add Timer to TrackPage. Add Button

--- client/src/routes/DisplayTrack/DisplayTrack.tsx
+++ client/src/routes/DisplayTrack/DisplayTrack.tsx
@@ -1,13 +1,18 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { StyledBanner } from 'src/components/Banner';
 import Page from 'src/layout/Page.styled';
 import BackgroundImage from 'src/assets/dev/7cDoyzS.jpg'
 import TrackInfoBox from 'src/components/TrackInfoBox';
 import { styled } from 'styled-components';
 import { flexContainer } from 'src/styles/mixins';
-import TrackPointNavigation from 'src/components/TrackPointNavigation';
+import TrackPointNavigation, { StyledTrackPointNavigation } from 'src/components/TrackPointNavigation';
+import StopWatch from 'src/components/StopWatch';
+import useTimer from 'src/hooks/useTimer';
+import ButtonIcon from 'src/components/Button/ButtonIcon';
 
 const DisplayTrack : FC = () => {
+    const [isRunning, setIsRunning] = useState(true);
+    const time = useTimer(isRunning);
     return (
         <Page>
             <TrackBanner background={BackgroundImage}>
@@ -15,6 +20,8 @@ const DisplayTrack : FC = () => {
             </TrackBanner>
             <InteractiveBar>
                 <TrackPointNavigation />
+                <ButtonIcon btnType='yellow' icon='start'>Rozpocznij grę</ButtonIcon>
+                <StopWatch time={time}/>
             </InteractiveBar>
         </Page>
     )
@@ -27,6 +34,11 @@ const TrackBanner = styled(StyledBanner)`
 const InteractiveBar = styled.div`
     ${flexContainer('space-between','center')};
     padding: 2rem 3.5rem;
+    gap: 2rem;
+
+    & > ${StyledTrackPointNavigation} {
+        flex: 1;
+    }
 `;
 
 export default DisplayTrack
